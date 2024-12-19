3b281e4de9372ab24c95ace87099d72517548542
JuanCarrion <Juanpablocarriondiaz@gmail.com>
2024-12-18 23:44:49 -0600

    about des

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,12 @@ function About() {
       <img className="about__picture" src={aboutPhoto} alt="about-photo"></img>
       <div className="about__text">
         <h2 className="about__title">About Juan Carrion</h2>
-        <p className="about__description">Talk about me etc.</p>
+        <p className="about__description">
+          My name is Juan Carrion, I'm a Junior Software Engineer who is
+          passionate about technology and software development. With a solid
+          foundation in programming languages, software engineering principles,
+          and problem-solving.
+        </p>
       </div>
     </section>
   );
